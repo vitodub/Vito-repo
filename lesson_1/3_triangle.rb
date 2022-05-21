26814aa4ea0989c2ce7cccee9905d80ef4ba3013
vitodub <105670453+vitodub@users.noreply.github.com>
2022-05-21 10:58:51 +0500

    Update 3_triangle.rb

--- lesson_1/3_triangle.rb
+++ lesson_1/3_triangle.rb
@@ -1,38 +1,30 @@
 t_sides = Array.new
 
-for side in 1..3 do
-  puts "Введите сторону #{side} треугольника"
-  t_sides[side - 1] = gets.to_f 
+def is_rectangular?(a, b, c)
+  (c**2).round == a**2 + b**2
+end
+
+3.times do |side|
+  puts "Введите сторону #{side + 1} треугольника"
+  t_sides[side] = gets.to_f 
 end
 
 t_sides.sort!
 
-def is_rectangular?(a, b, c)
-  (c ** 2).round == a ** 2 + b ** 2
-end
-
-repeats = t_sides.select{|side| t_sides.count(side) > 0}.uniq
+repeats = t_sides.uniq
 
 if repeats.length == 1
-  
   puts "Треугольник равносторонний"
-  
 elsif repeats.length == 2
-  
   if is_rectangular?(t_sides[0], t_sides[1], t_sides[2])
     puts "Треугольник равнобедренный и прямоугольный"
-    
   else 
     puts "Треугольник равнобедренный"
   end
-  
 else
-  
   if is_rectangular?(t_sides[0], t_sides[1], t_sides[2])
     puts "Треугольник прямоугольный"
-
   else
     puts "Треугольник неравносторонний, неравнобедренный, непрямоугольный"
   end   
-  
 end
