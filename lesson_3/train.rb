249da0953fe7c781d4321e920170ba0cc1f44787
vitodub <105670453+vitodub@users.noreply.github.com>
2022-05-28 18:56:51 +0500

    Add files via upload

---
+++ lesson_3/train.rb
@@ -0,0 +1,47 @@
+class Train
+  attr_reader :type, :speed, :wagon_amount, :current_station
+
+  def initialize(number, type, wagon_amount)
+    @number = number
+    @type = type
+    @wagon_amount = wagon_amount
+    @speed = 0
+  end
+
+  def speed_up(speed)
+    @speed = speed
+  end
+
+  def stop
+    @speed = 0
+  end
+
+  def add_wagon
+    @speed == 0 ? @wagon_amount +=1 : (puts "Поезд движется. Это невозможно")
+    end
+
+  def delete_wagon
+    @speed == 0 ? @wagon_amount -=1 : (puts "Поезд движется. Это невозможно")
+  end
+
+  def add_route(route)
+    @route = route
+    @current_station = @route[0]
+  end
+
+  def move_to_next_station
+    @current_station == @route[-1] ? (puts "Эта станция конечная") : @current_station = @route[@route.index(@current_station) + 1]
+  end
+
+  def move_to_previous_station
+    @current_station == @route[0] ? (puts "Эта станция начальная") : @current_station = @route[@route.index(@current_station) - 1]
+  end
+
+  def show_next_station
+    @current_station == @route[-1] ? (puts "Эта станция конечная") : @route[@route.index(@current_station) + 1]
+  end
+
+  def show_previous_station
+    @current_station == @route[0] ? (puts "Эта станция начальная") : @route[@route.index(@current_station) - 1]
+  end
+end
