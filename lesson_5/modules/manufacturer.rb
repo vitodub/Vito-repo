b88dd49619440afda13820f5020e49b67dec2894
vitodub <105670453+vitodub@users.noreply.github.com>
2022-06-19 21:44:57 +0500

    Update manufacturer.rb

--- lesson_5/modules/manufacturer.rb
+++ lesson_5/modules/manufacturer.rb
@@ -1,12 +1,3 @@
 module Manufacturer
-  def set_manufacturer(name)
-    self.manufacturer = name
-  end
-
-  def get_manufacturer
-    self.manufacturer
-  end
-
-  protected
   attr_accessor :manufacturer
 end
