e1a82b89745337e8f85b4b337b4f899fc7bfb01d
vitodub <105670453+vitodub@users.noreply.github.com>
2022-06-25 17:44:50 +0500

    Add files via upload

---
+++ lesson_7/modules/instance_сounter.rb
@@ -0,0 +1,20 @@
+module InstanceCounter
+  def self.included(base)
+    base.extend ClassMethods
+    base.include InstanceMethods
+  end
+
+  module ClassMethods
+    attr_reader :instances
+
+    
+    attr_writer :instances
+  end
+
+  module InstanceMethods
+    private    
+    def register_instance
+      self.class.instances = self.class.instances.nil? ? 1 : self.class.instances + 1
+    end  
+  end
+end
---
+++ lesson_7/modules/validation.rb
@@ -0,0 +1,8 @@
+module Validation
+  def valid?
+    validate!
+  rescue
+    false
+  end
+
+end
