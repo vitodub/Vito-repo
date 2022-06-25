4755efcb02d2eea2f20f71dc80b37730b77a3151
vitodub <105670453+vitodub@users.noreply.github.com>
2022-06-25 17:43:00 +0500

    Add files via upload

---
+++ lesson_7/main.rb
@@ -0,0 +1,11 @@
+require './classes/station.rb'
+require './classes/train.rb' 
+require './classes/route.rb'
+require './classes/wagon.rb'
+require './classes/passenger_wagon.rb'
+require './classes/cargo_wagon.rb'
+require './classes/passenger_train.rb'
+require './classes/cargo_train.rb'
+require './classes/interface.rb'
+
+Interface.new.call
