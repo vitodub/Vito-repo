5c0f660fdf4e1a2db4e46e4900756cc5d15c0a7e
vitodub <105670453+vitodub@users.noreply.github.com>
2022-07-31 23:13:18 +0500

    Update Validation.rb

--- Lesson_10/Validation.rb
+++ Lesson_10/Validation.rb
@@ -1,6 +1,4 @@
-# encoding: utf-8
-
-module Validation
+odule Validation
 
   def self.included(base)
     base.extend ClassMethods
@@ -8,27 +6,35 @@ module Validation
   end
 
   module ClassMethods
-  attr_accessor :a_name, :validation, :parametr
 
-    def validate(a_name, validation, parametr = nil)
-      @a_name = a_name
-      @validation = validation
-      @parametr = parametr
+    def validations
+      @validations ||= []
+    end
+    # создаем геттер для массива, куда мы складываем валидации. Складываем в переменную экземпляра Class-уровня
+  
+    def validate(a_name, validation_type, parametr = nil)
+      validation = { a_name: a_name, validation_type: validation_type, parametr: parametr } 
+      instance_variable_set(:@validations, validations << validation) #Добавляем наши валидации к ранее созданной переменной экземпляра Class-уровня
     end
   end
 
   module InstanceMethods
 
     def validate!
-      case self.class.validation
-      when "presence"
-        raise "Значение атрибута не может быть nil или пустой строкой" if (self.class.a_name.nil? || self.class.a_name == '')
-      when "format"
-        raise "Значение атрибута не соответствует заданному формату" if self.class.a_name !~ self.class.parametr
-      when "type"
-        raise "Тип присваемоего значения отличается" unless self.class.a_name == self.class.parametr 
-      else
-        puts "Задан неверный тип проверки. Доступные варианты: presence, format, type"
+      self.class.validations.each do |parametrs|
+        a_name, validation_type, parametr = parametrs.values
+        value = instance_variable_get("@#{a_name}".to_sym) #обращаемся к переменной экземпляра класса
+
+        case validation_type
+        when :presence
+          raise "Значение атрибута не может быть nil или пустой строкой" if value.nil? || value == ''
+        when :format
+          raise "Значение атрибута не соответствует заданному формату" if value !~ parametr
+        when :type
+          raise "Тип присваемоего значения отличается" unless value.is_a?(parametr)
+        else
+          puts "Задан неверный тип проверки. Доступные варианты: presence, format, type"
+        end
       end
     end
 
