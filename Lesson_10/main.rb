a7c47d134ec0f9d6a134c78ccd650f20e17bcce4
vitodub <105670453+vitodub@users.noreply.github.com>
2022-07-31 23:16:25 +0500

    Add files via upload

---
+++ Lesson_10/main.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require './classes/station'
+require './classes/train'
+require './classes/route'
+require './classes/wagon'
+require './classes/passenger_wagon'
+require './classes/cargo_wagon'
+require './classes/passenger_train'
+require './classes/cargo_train'
+require './classes/interface'
+
+Interface.new.call
