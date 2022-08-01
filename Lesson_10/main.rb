966b1e589a1c70b015c57ce42c953f3a32d93449
vitodub <105670453+vitodub@users.noreply.github.com>
2022-08-01 13:26:26 +0500

    Update main.rb

--- Lesson_10/main.rb
+++ Lesson_10/main.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require './classes/station'
-require './classes/train'
-require './classes/route'
-require './classes/wagon'
-require './classes/passenger_wagon'
-require './classes/cargo_wagon'
-require './classes/passenger_train'
-require './classes/cargo_train'
-require './classes/interface'
+require_relative './classes/station'
+require_relative './classes/train'
+require_relative './classes/route'
+require_relative './classes/wagon'
+require_relative './classes/passenger_wagon'
+require_relative './classes/cargo_wagon'
+require_relative './classes/passenger_train'
+require_relative './classes/cargo_train'
+require_relative './classes/interface'
 
 Interface.new.call
