5c0a77ca69e0a367c0f62c166ec8c07d871e0669
vitodub <105670453+vitodub@users.noreply.github.com>
2022-08-01 13:27:40 +0500

    Update train.rb

--- Lesson_10/classes/train.rb
+++ Lesson_10/classes/train.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative '../modules/instance_сounter' 
-require_relative '../modules/validation'
-require_relative '../modules/manufacturer'
+require_relative './modules/instance_сounter' 
+require_relative './modules/validation'
+require_relative './modules/manufacturer'
 
 class Train
   NUMBER_FORMAT = /^[a-z0-9]{3}(-| )[a-z0-9]{2}$/i.freeze
